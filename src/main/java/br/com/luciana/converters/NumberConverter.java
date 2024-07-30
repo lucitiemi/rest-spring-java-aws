b1629fb0d3aa2ab7b05a9e5ddd2d064b3af5a486
Luciana <lt.shimoyama@gmail.com>
2024-07-30 18:41:06 -0300

    Calculadora funcionando

--- src/main/java/br/com/luciana/converters/NumberConverter.java
+++ src/main/java/br/com/luciana/converters/NumberConverter.java
@@ -3,7 +3,6 @@ package br.com.luciana.converters;
 public class NumberConverter {
 
 	
-	// metodo auxiliar - CONVERT TO DOUBLE
 	public static Double convertToDouble(String strNumber) {
 		if (strNumber == null) return 0D;
 		String number = strNumber.replaceAll(",", ".");
@@ -12,7 +11,6 @@ public class NumberConverter {
 	}
 
 
-	// metodo auxiliar - IS NUMERIC
 	public static boolean isNumeric(String strNumber) {
 		if (strNumber == null) return false;
 		String number = strNumber.replaceAll(",", ".");
