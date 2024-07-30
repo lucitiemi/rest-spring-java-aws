03e41320bdd9c34dbe09e2af3be4df821daecdb9
Luciana <lt.shimoyama@gmail.com>
2024-07-30 16:41:52 -0300

    First steps in Java with Spring Boot

---
+++ rest-spring-java-aws/rest-spring-java-aws/src/test/java/br/com/luciana/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.luciana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
